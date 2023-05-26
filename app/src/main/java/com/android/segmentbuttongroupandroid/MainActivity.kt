03fbfe0881affdd6bbcb3226657f5410cbfda74b
“Aashis” <aashis.thapa@ekbana.info>
2023-05-26 18:55:05 +0545

    first commit

---
+++ app/src/main/java/com/android/segmentbuttongroupandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.android.segmentbuttongroupandroid
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
